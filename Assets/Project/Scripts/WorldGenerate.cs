cfc2c996f4e1997f638e2adb7931211969c44f7d
Cpt Crouch <macrez.theo@hotmail.fr>
2016-12-30 16:36:44 +0100

    Push 30/12/2016 : New Editor et Move Auto

--- Assets/Project/Scripts/WorldGenerate.cs
+++ Assets/Project/Scripts/WorldGenerate.cs
@@ -58,7 +58,7 @@ public class WorldGenerate : MonoBehaviour {
         {
             if (Application.isEditor ==  false)
             {
-                GenerateHexagonWorld(rayon);
+                //GenerateHexagonWorld(rayon);
                 
             }
             else
